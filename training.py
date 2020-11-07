f62aade025c516663423ffe93deb92c589873630
Timo Achtelik <timo.achtelik@stud.h-da.de>
2020-11-07 17:39:25 +0100

    latest update

--- training.py
+++ training.py
@@ -10,6 +10,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
+import re
+from nltk.corpus import wordnet
 
 words=[]
 classes = []
